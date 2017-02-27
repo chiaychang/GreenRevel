e8ac616dd3867b59ae0ef18af4031d466f7f9b53
Joy Chang <joychang@joyteki-mbp.attlocal.net>
2017-02-26 19:22:53 -0600

    debug

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include_once("homepage/index.html"); ?>
+<?php include_once("aboutpage/index.html"); ?>
+<?php include_once("shopitem/index.html"); ?>
+<?php include_once("shoppage/index.html"); ?>
+<?php include_once("contactpage/contact.html"); ?>
